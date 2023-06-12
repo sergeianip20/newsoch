751fe074c9e8d1a0704baa01370b3292bef9303e
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-06-12 18:53:05 +0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {store} from "./state/store";
 import { Provider } from 'react-redux';
-
+import {createHashRouter} from 'react-router-dom'
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
