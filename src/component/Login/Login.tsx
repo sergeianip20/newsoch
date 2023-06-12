0bc37a3d1eed6c9440dc2f735fdc4ce506d3d669
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-06-12 19:13:11 +0400

    Update Login.tsx
    Login

--- src/component/Login/Login.tsx
+++ src/component/Login/Login.tsx
@@ -1,4 +1,66 @@
 import React from 'react'
+ProComponentsProComponents
+
+Login form
+
+LoginForm/Page
+
+LoginForm and LoginFormPage are variants of ProForm. They are specially implemented to adapt to common login form layouts. They are suitable for various login scenarios and reduce layout pressure.
+
+Login form
+
+Github
+
+全球最大的代码托管平台
+
+账号密码登录
+
+手机号登录
+
+自动登录
+
+忘记密码
+
+
+
+import {
+
+  AlipayCircleOutlined,
+
+  LockOutlined,
+
+  MobileOutlined,
+
+  TaobaoCircleOutlined,
+
+  UserOutlined,
+
+  WeiboCircleOutlined,
+
+} from '@ant-design/icons';
+
+import {
+
+  LoginForm,
+
+  ProConfigProvider,
+
+  ProFormCaptcha,
+
+  ProFormCheckbox,
+
+  ProFormText,
+
+} from '@ant-design/pro-components';
+
+import { message, Space, Tabs } from 'antd';
+
+import type { CSSProperties } from 'react';
+
+import { useState } from 'react';
+
+type LoginType = 'phone' | 'account';
+
 
 export const Login=React.memo(()=> {
     return <div>
