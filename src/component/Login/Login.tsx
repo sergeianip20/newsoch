9b34cb6ec80498ce65a4b88b27f43cdcb3e0decd
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-06-12 20:12:11 +0400

    Update Login.tsx

--- src/component/Login/Login.tsx
+++ src/component/Login/Login.tsx
@@ -1,32 +1,4 @@
-import React from 'react'
-import {
-  AlipayCircleOutlined,
-  LockOutlined,
-  MobileOutlined,
-  TaobaoCircleOutlined,
-  UserOutlined,
-  WeiboCircleOutlined,
-} from '@ant-design/icons';
-import {
-  LoginForm,
-  ProConfigProvider,
-  ProFormCaptcha,
-  ProFormCheckbox,
-  ProFormText,
-} from '@ant-design/pro-components';
 
-import { message, Space, Tabs } from 'antd';
-import type { CSSProperties } from 'react';
-import { useState } from 'react';
-
-type LoginType = 'phone' | 'account';
-
-
-export const Login=React.memo(()=> {
-    return <div>
- hello
-    </div>
-})
 import {
 
   AlipayCircleOutlined,
@@ -79,7 +51,7 @@ const iconStyles: CSSProperties = {
 
 };
 
-export default () => {
+export const Login=React.memo(()=> {
 
   const [loginType, setLoginType] = useState<LoginType>('phone');
 
@@ -331,4 +303,4 @@ export default () => {
 
   );
 
-};
+})
