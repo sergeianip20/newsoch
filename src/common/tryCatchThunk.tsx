ae3285ab9d4f9f459d03a3f6ce2b6313fbd3f8ff
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-06-15 10:32:43 +0400

    Update tryCatchThunk.tsx
    AppRootStateType

--- src/common/tryCatchThunk.tsx
+++ src/common/tryCatchThunk.tsx
@@ -1,11 +1,11 @@
 import {BaseThunkAPI} from "@reduxjs/toolkit/dist/createAsyncThunk";
-import {AppDispatch, RootState} from "state/store";
+import {AppDispatch, AppRootStateType} from "state/store";
 import {AxiosError, isAxiosError} from "axios";
 
 
 
 export const thunkTryCatch = async (
-    thunkAPI: BaseThunkAPI<RootState, any, AppDispatch, unknown>,
+    thunkAPI: BaseThunkAPI<AppRootStateType, any, AppDispatch, unknown>,
     logic: Function,
     showGlobalError: boolean = true
 ) => {
