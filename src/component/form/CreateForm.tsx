9b30e04b7e7f389ab8809fc15ed3a90a7fa57d4f
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-06-16 17:11:09 +0400

    Update CreateForm.tsx

--- src/component/form/CreateForm.tsx
+++ src/component/form/CreateForm.tsx
@@ -9,7 +9,11 @@ export const ProfileForm=()=> {
 return( <>
 <Row>
   <Card title='Изменения профиля' bordered={false} style={{width:1200}}>
-    
+    <Row> 
+      <Col>
+        <Input />
+      </Col>
+    </Row>
   </Card>
 </Row>
 </>)
