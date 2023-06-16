71d189109ca4cbdbc9bf39a56465fd11cb4ed412
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-06-16 16:35:57 +0400

    Create CreateForm.tsx

---
+++ src/component/form/CreateForm.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Input, Row, Col, Card} from 'ant'
+import {useForm, SubmitHandler, Path, UseFormRegister} from "react-hook-form";
+
+
+export const ProfileForm=()=> {
+  
+
+return <>
+<Row>
+  <Card title='Изменения профиля' bordered={false} style={{width:1200}}>
+    
+  </Card>
+</Row>
+</>
+}
