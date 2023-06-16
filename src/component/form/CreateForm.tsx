d73b72f4555b8b5a7e67a77bd4b3e2c3f2a94e05
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-06-16 17:03:41 +0400

    Update CreateForm.tsx

--- src/component/form/CreateForm.tsx
+++ src/component/form/CreateForm.tsx
@@ -6,11 +6,11 @@ import {Input, Row, Col, Card} from 'antd'
 export const ProfileForm=()=> {
   
 
-return <>
+return( <>
 <Row>
   <Card title='Изменения профиля' bordered={false} style={{width:1200}}>
     
   </Card>
 </Row>
-</>
+</>)
 }
