8805c7e9592fd3b4cdb8df0b7597ebee6d1e5ca5
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-06-16 10:35:57 +0400

    Update Profile.tsx

--- src/component/profile/Profile.tsx
+++ src/component/profile/Profile.tsx
@@ -1,9 +1,13 @@
 import {Image, Col, Row} from 'antd'
-
-export const Profile =()=> {
+import React, {FC} from 'react'
+export const Profile:FC =()=> {
 
 
     return <div>
-  
+    <Row>
+        
+        <Col span={24}>
+            <Image      width={200}            /> </Col>
+    </Row>
     </div>
 }
